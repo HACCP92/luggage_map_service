9b3fd1a29005e7f827cced259f58b5c634ce1723
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-10-24 23:33:49 +0900

    [물품보관소 맵 서비스] Naver maps 초기 설정

--- android/app/src/main/kotlin/com/example/luggage_map_service/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/luggage_map_service/MainActivity.kt
@@ -3,4 +3,8 @@ package com.example.luggage_map_service
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
 }
