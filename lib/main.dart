4d615952330746c351165a56c9b406399345fa00
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-10-25 23:32:12 +0900

    [물품보관소 맵 서비스] naver_maps 적용해서 main 재작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-void main() {
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //어떤것을 준비하는데 기달려준다!
+  await NaverMapSdk.instance.initialize(clientId: 'wguyhkm25y');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
